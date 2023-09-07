8567c5161405a3b7079750c78ebdb7c511553b45
BinciLuo <binciluo@icloud.com>
2023-09-07 17:47:31 +0800

    [refactor] rm unused data

--- api.py
+++ api.py
@@ -1,29 +1,10 @@
 import torch
-from preprocess import label_to_id,id_to_label
-from preprocess import Vocabulary
-import mdtex2html
 import gradio as gr
 
 # 加载模型
 """
 
 """
-vocab = Vocabulary()
-vocab.load_vocabulary()
-
-device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-load_path='./res_model/model_epoch8.pth'
-model=torch.load(load_path,map_location=device)
-model.eval()
-
-id2label=id_to_label()
-
-def text_to_id(text, max_len):
-    text_ids = vocab.text_to_ids(text)
-    lenx = len(text_ids)
-    if lenx < max_len: text_ids += [0 for _ in range(max_len - lenx)]
-    else: text_ids = text_ids[0:max_len]
-    return torch.tensor([text_ids])
 
 
 # 定义推理函数
@@ -31,16 +12,8 @@ def text_to_id(text, max_len):
 
 """
 def infer(infer_text):
-    inputs=text_to_id(infer_text,20)
-    with torch.no_grad():
-        inputs=inputs.to(device)
-        preds = model(inputs)
-        all_preds=[preds]
-        all_preds = torch.cat(all_preds, dim=0)
-        label_idx=all_preds.argmax(dim=1)
-        infered_label = id2label[int(label_idx)]
-        print(infer_text,infered_label,flush=True)
-        return infered_label
+    infered_label=""
+    return infered_label
 
 
 # API
