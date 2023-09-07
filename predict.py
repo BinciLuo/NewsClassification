33ccf7ffef50b8f8a695833968a6f802f06c1b04
lyk <lyk>
2023-09-07 20:35:39 +0800

    forced cuda solved

--- predict.py
+++ predict.py
@@ -6,13 +6,13 @@ import torch
 
 
 
-
 # %%
 
 def init():
-    global model, idx2label, label2idx, wvm, embedding_matrix
+    global model, idx2label, label2idx, wvm, embedding_matrix, device
     model = torch.load('pth/epoch_4.pth')
-    model = model.to('cuda')
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = model.to(device)
     model.eval()
     idx2label = pickle_load('idx2label')
     label2idx = pickle_load('label2idx')
@@ -22,17 +22,16 @@ def init():
 
 def predict(text):
     with torch.no_grad():
-        test_outputs = model(torch.LongTensor([text_to_id(text, wvm, 30)]).to('cuda'))
+        test_outputs = model(torch.LongTensor([text_to_id(text, wvm, 30)]).to(device))
         _, predicted = torch.max(test_outputs, 1)
         predicted = predicted.to('cpu').numpy()
-        print(predicted)
         predicted = idx2label[predicted[0]]
     return predicted
 
 # %%
 
 init()
-predict(input())
+print(predict('世界杯四强诞生！FIBA第一人出局！美国队迎挑战，最强黑马崛起'))
 
 # %%
 
